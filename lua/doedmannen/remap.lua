2e91136132d792c76f0faf71a6364d3a2cde6bf9
Kristoffer Katra <kristoffer.katra@coolstuff.com>
2025-09-13 00:35:40 +0200

    add some remaps

--- lua/doedmannen/remap.lua
+++ lua/doedmannen/remap.lua
@@ -1,9 +1,21 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-
 vim.g.maplocalleader = "\\"
 
+-- if not using oil as the explorer
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- move line up or down
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
+-- global copy
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+-- black hole deletion
+vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
+vim.keymap.set({"n", "v"}, "<leader>c", [["_c]])
+
+-- default format
+vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 
