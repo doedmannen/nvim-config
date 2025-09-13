154644d24c271d1d76cb1dcfe4ab0fd00c622411
Kristoffer Katra <kristoffer.katra@coolstuff.com>
2025-09-13 22:25:48 +0200

    remap show diagnostics

--- lua/doedmannen/remap.lua
+++ lua/doedmannen/remap.lua
@@ -4,18 +4,22 @@ vim.g.maplocalleader = "\\"
 -- if not using oil as the explorer
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
--- move line up or down
+-- move line up or down in visual mode
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
--- global copy
+-- yank to OS clipboard
 vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
--- black hole deletion
+-- black hole delete/replace
 vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
 vim.keymap.set({"n", "v"}, "<leader>c", [["_c]])
 
 -- default format
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+
+-- show diagnostics
+vim.keymap.set("n", "<leader>sd", vim.diagnostic.open_float)
+
 
