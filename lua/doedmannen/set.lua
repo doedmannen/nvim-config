934f3785a8ee755ca4263da1d5f2dfc28f8205e5
Kristoffer Katra <kristoffer.katra@coolstuff.com>
2025-09-13 22:25:37 +0200

    add inline diagnostics

--- lua/doedmannen/set.lua
+++ lua/doedmannen/set.lua
@@ -14,18 +14,27 @@ vim.opt.wrap = false
 
 vim.opt.swapfile = false
 vim.opt.backup = false
--- vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
--- vim.opt.undofile = true
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
 
-vim.opt.hlsearch = true
+vim.opt.hlsearch = true -- highlight search
 vim.opt.incsearch = true
 
 vim.opt.termguicolors = true
 
 vim.opt.scrolloff = 8
-vim.opt.signcolumn = "yes"
-vim.opt.isfname:append("@-@")
+vim.opt.signcolumn = "yes" -- always keep sign column open
+vim.opt.isfname:append("@-@") -- allow @ in file names
 
 vim.opt.updatetime = 50
 
 vim.opt.colorcolumn = "80"
+
+-- start show inline diagnostics:
+vim.diagnostic.config({
+  virtual_text = true,  -- Shows diagnostics as virtual text at end of line
+  signs = true,         -- Shows signs in sign column
+  underline = true,     -- Underlines problematic text
+  update_in_insert = false,  -- Don't update while typing
+})
+-- end show inline diagnostics
