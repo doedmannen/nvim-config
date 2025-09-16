5f072741a840161b0ce3b118cc7044e8b0d40943
Kristoffer Katra <kristoffer.katra@coolstuff.com>
2025-09-16 21:21:48 +0200

    ignore errors for highlight on deleted lines

--- lua/doedmannen/highlight-yank.lua
+++ lua/doedmannen/highlight-yank.lua
@@ -2,10 +2,12 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
   group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
   callback = function()
-    vim.highlight.on_yank({
-      higroup = 'IncSearch',     -- highlight group to use
-      timeout = 300,          -- how long to show highlight (ms)
-    })
+    pcall(function() -- protected call suppresses errors for highlighting on delete yanks
+      vim.highlight.on_yank({
+        higroup = 'IncSearch',     -- highlight group to use
+        timeout = 300,          -- how long to show highlight (ms)
+      })
+    end)
   end,
 })
 
