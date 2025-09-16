92b248fe26b40ab2cb46b9f39da3e1a506da7c1a
Kristoffer Katra <kristoffer.katra@coolstuff.com>
2025-09-16 21:20:07 +0200

    stop using undo file

--- lua/doedmannen/set.lua
+++ lua/doedmannen/set.lua
@@ -16,8 +16,8 @@ vim.opt.wrap = false
 
 vim.opt.swapfile = false
 vim.opt.backup = false
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
-vim.opt.undofile = true
+--vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+--vim.opt.undofile = true
 
 vim.opt.hlsearch = true -- highlight search
 vim.opt.incsearch = true
