a3da88ca6b77817652848725cfef81bfeb5dc2af
doedmannen <kristofferkatra@gmail.com>
2025-09-26 22:56:00 +0200

    setup mason

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,3 +1,19 @@
 return {
   "neovim/nvim-lspconfig",
+  event = { "BufReadPre", "BufNewFile" },
+  dependencies = {
+    "williamboman/mason.nvim",
+    "williamboman/mason-lspconfig.nvim",
+  },
+  config = function()
+    require("mason").setup()
+    require("mason-lspconfig").setup({
+      automatic_installation = true,
+      handlers = {
+        function(server_name)
+          require("lspconfig")[server_name].setup({})
+        end,
+      },
+    })
+  end
 }
