5a9d6dede5e86b389a9c27d6ba9c6082366b8978
Kristoffer Katra <kristoffer.katra@coolstuff.com>
2025-09-26 08:37:03 +0200

    change colorscheme to gruvbox material

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,7 +1,22 @@
+-- gruvbox material
 return {
-  "rose-pine/neovim",
-  name = "rose-pine",
+  'sainnhe/gruvbox-material',
+  lazy = false,
+  priority = 1000,
   config = function()
-    vim.cmd("colorscheme rose-pine")
+    -- Optionally configure and load the colorscheme
+    -- directly inside the plugin declaration.
+    vim.g.gruvbox_material_enable_italic = true
+    vim.cmd.colorscheme('gruvbox-material')
   end
 }
+
+
+-- rosepine
+-- return {
+--   "rose-pine/neovim",
+--   name = "rose-pine",
+--   config = function()
+--     vim.cmd("colorscheme rose-pine")
+--   end
+-- }
