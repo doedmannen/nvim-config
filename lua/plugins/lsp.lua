02800c8737b8617ca93856b4801872f8c55c41b1
doedmannen <kristofferkatra@gmail.com>
2025-09-27 22:48:02 +0200

    add lazydev dependency in lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -8,6 +8,17 @@ return {
       "j-hui/fidget.nvim",
       tag = "v1.6.1",
       opts = {}
+    },
+    {
+      "folke/lazydev.nvim",
+      ft = "lua", -- only load on lua files
+      opts = {
+        library = {
+          -- See the configuration section for more details
+          -- Load luvit types when the `vim.uv` word is found
+          { path = "luvit-meta/library", words = { "vim%.uv" } },
+        },
+      },
     }
   },
   config = function()
