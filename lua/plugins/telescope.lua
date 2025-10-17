93e1f5acef48832208e40c9e095bb38771377500
Kristoffer Katra <kristoffer.katra@coolstuff.com>
2025-10-17 09:36:13 +0200

    resume latest search buffer

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,6 +4,7 @@ vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live gr
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
 vim.keymap.set('n', '<leader>gf', builtin.git_files, { desc = 'Find in Git files' })
+vim.keymap.set('n', '<leader>fl', builtin.resume, { desc = 'Open latest search' })
 vim.keymap.set('n', '<leader>fe', function()
   builtin.find_files({
     prompt_title = "Find .env files",
